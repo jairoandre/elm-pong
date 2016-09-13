bf1a67c5ee9b511de44129977b256d1ccce8fc8f
Jairo Oliveira <Jairo Oliveira>
2016-09-13 01:52:00 -0300

    Perspective

--- Experiments.elm
+++ Experiments.elm
@@ -1,10 +1,10 @@
 module Experiments exposing (..)
 
 import Html.App as App
-import Html exposing (Html, div)
+import Html exposing (Html, div, text)
 import Color exposing (rgb)
 import Graphics.Render as Render
-import Time
+import AnimationFrame
 
 
 main : Program Never
@@ -22,14 +22,30 @@ main =
 
 
 type alias Model =
-    { angle : Int
-    , board : List Render.Point
+    { angle : Float
+    , board : List Vec3
     }
+
+
+type alias Vec3 =
+    ( Float, Float, Float )
+
+
+toRenderPoint : Vec3 -> Render.Point
+toRenderPoint vec3 =
+    let
+        ( x, y, z ) =
+            vec3
+
+        dz =
+            z + 50
+    in
+        ( x / dz, y / dz )
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { angle = 0, board = [ ( 0, 0 ), ( 0, 100 ), ( 100, 100 ), ( 100, 0 ) ] }, Cmd.none )
+    ( { angle = 0.001, board = [ ( -100, -100, 0 ), ( -100, 100, 0 ), ( 100, 100, 0 ), ( 100, -100, 0 ) ] }, Cmd.none )
 
 
 
@@ -40,11 +56,60 @@ type Msg
     = Tick Float
 
 
+updateBoard : Model -> List Vec3
+updateBoard model =
+    List.map
+        (\point ->
+            let
+                ( x, y, z ) =
+                    point
+
+                ca =
+                    cos model.angle
+
+                sa =
+                    sin model.angle
+
+                cx =
+                    x * ca
+
+                cy =
+                    (y - 50) * ca
+
+                cz =
+                    (z - 50) * ca
+
+                sx =
+                    x * sa
+
+                sy =
+                    (y - 50) * sa
+
+                sz =
+                    (z - 50) * sa
+
+                zz =
+                    z
+
+                newX =
+                    x
+
+                newY =
+                    cy + sz
+
+                newZ =
+                    cz - sy
+            in
+                ( newX, newY, newZ )
+        )
+        model.board
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
         Tick time ->
-            ( { model | angle = model.angle + 10 }, Cmd.none )
+            ( { model | board = updateBoard model }, Cmd.none )
 
 
 
@@ -53,7 +118,7 @@ update message model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every Time.second Tick
+    AnimationFrame.diffs Tick
 
 
 
@@ -62,7 +127,7 @@ subscriptions model =
 
 board : Model -> Render.Form msg
 board model =
-    Render.polygon model.board
+    Render.polygon (List.map toRenderPoint model.board)
         |> Render.solidFill (rgb 255 0 0)
 
 
@@ -70,5 +135,5 @@ view : Model -> Html Msg
 view model =
     div []
         [ board model
-            |> Render.svg 400 400
+            |> Render.svg 500 500
         ]
